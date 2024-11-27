0f3e3134bda1900fe9983e4d63ef386f658140bd
melrwa <orwamelkizedek@gmail.com>
2024-11-27 19:38:17 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
-
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
 
-    #Instance method definition
-    pass
+#creating an instance for fido
+fido = Dog()
+fido.bark()
+fido.sit()
+
+#Creating another instance for snoopy
+snoopy = Dog()
+snoopy.bark()
+snoopy.sit()
+
